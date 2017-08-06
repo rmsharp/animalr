5911c7a029e711a35bfe29e6b0c5a52eba0c1bc5
rmsharp <rmsharp@me.com>
2017-08-06 16:53:37 -0500

    Added is_database_view

---
+++ R/is_database_view.R
@@ -0,0 +1,17 @@
+#' Returns TRUE if view exist and FALSE if it does not
+#'
+#' @param conn database connection object
+#' @param view_name character vector of length one having the name of the
+#' view.
+#' @param schema character vector of length one having the name of the
+#' schema
+#' @import RODBC
+#' @import stringi
+#' @export
+is_database_tbl <- function(conn, view_name, schema = "DBO") {
+  sql_txt <- stri_c(
+    "SELECT count(1) FROM INFORMATION_SCHEMA.VIEWS
+    WHERE TABLE_NAME='", view_name, "'
+      AND TABLE_SCHEMA = '", schema, "') ")
+  sqlQuery(conn, sql_txt)$result > 0
+}
