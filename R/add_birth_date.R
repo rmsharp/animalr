f4c79626a4b9bb1c20edae23b9408e6a480e5c20
R. Mark Sharp <msharp@TxBiomed.org>
2017-09-07 16:11:54 -0500

    separated out add_birth_date() and added add_death_date()

---
+++ R/add_birth_date.R
@@ -0,0 +1,20 @@
+#' Returns dataframe with POSIXct date vector of birth dates for each animal
+#' in character vector id of the provided dataframe id_df.
+#' Returns NA in vector if not available.
+#'
+#' Assumes access to master database table in animal database.
+#' @param conn database connection
+#' @param id_df dataframe with animal Ids in id column
+#' @import RODBC
+#' @import rmsutilityr
+#' @import stringi
+#' @export
+add_birth_date <- function(conn, id_df) {
+  ids <- id_df$id[!is.na(id_df$id)]
+  ids_str <- vector2string(unique(blank_fill_ids(ids)), SS = "','")
+  birth_dates_df <- sqlQuery(conn, stri_c(
+    "SELECT id, birth_date from master where id in ('", ids_str, "')"))
+  birth_dates_df$id <- blank_fill_ids(birth_dates_df$id)
+  id_df$id <- blank_fill_ids(id_df$id)
+  merge(id_df, birth_dates_df, by = 'id', sort = TRUE, all = TRUE)
+}
--- R/animalr.R
+++ R/animalr.R
@@ -439,26 +439,6 @@ get_vasectomized_males <-
       where a.attribute = 'vasectomized'")
     sqlQuery(conn, sql_txt, stringsAsFactors = FALSE)
 }
-#' Returns dataframe with POSIXct date vector of birth dates for each animal
-#' in character vector id of the provided dataframe id_df.
-#' Returns NA in vector if not available.
-#'
-#' Assumes access to master database table in animal database.
-#' @param conn database connection
-#' @param id_df dataframe with animal Ids in id column
-#' @import RODBC
-#' @import rmsutilityr
-#' @import stringi
-#' @export
-add_birth_date <- function(conn, id_df) {
-  ids <- id_df$id[!is.na(id_df$id)]
-  ids_str <- vector2string(unique(blank_fill_ids(ids)), SS = "','")
-  birth_dates_df <- sqlQuery(conn, stri_c(
-    "SELECT id, birth_date from master where id in ('", ids_str, "')"))
-  birth_dates_df$id <- blank_fill_ids(birth_dates_df$id)
-  id_df$id <- blank_fill_ids(id_df$id)
-  merge(id_df, birth_dates_df, by = 'id', sort = TRUE, all = TRUE)
-}
 #' Returns dataframe with colony for each animal
 #' in character vector id for a specified date in colony_date column of the
 #' provided dataframe id_df.
